cc3f3b79671885028a13086be59eeb926087d1d1
rexlin <rexlin6245@gmail.com>
2021-05-18 22:47:51 -0700

    second time submit

--- POS.js
+++ POS.js
@@ -13,11 +13,29 @@ const view = {
         </div>
       `
     })
+  },
+
+  renderOption (className) {
+    let domObj = document.querySelector(`.${className}`)
+    domObj.innerHTML = `<div class="selection">${className} : </div>`
+    model[className].forEach(item => {
+      return domObj.innerHTML += `
+        <label class="btn btn-outline-primary">
+          <input type="radio" name="${className}" value="${item}" />${item}
+        </label>
+      `
+    })
   }
 }
 
 const model = {
   drinkList: ['Milk tea', 'Black tea', 'Green tea', 'Lemon tea', 'Oolong tea', 'Matcha latte', 'London fog', 'Coffee latte'],
+
+  type: ['cold', 'hot'],
+
+  ice: ['no ice', 'less ice', 'half ice', 'normal'],
+
+  sugar: ['no sugar', 'less sugar', 'half sugar', 'normal'],
 
   drinkPrice (drinkName) {
     switch (drinkName) {
@@ -39,3 +57,6 @@ const model = {
 }
 
 view.renderDrinkList()
+view.renderOption('type')
+view.renderOption('ice')
+view.renderOption('sugar')
